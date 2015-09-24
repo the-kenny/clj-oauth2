e69e70c47aa5a263f516fcaa46eb5289345a5c30
Stuart Hinson <stuart.hinson@gmail.com>
2015-09-24 18:54:16 -0400

    tidy project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,3 @@
-(def dev-dependencies
-  '[[ring "0.3.11"]])
-
 (defproject clj-oauth2 "0.4.0-SNAPSHOT"
   :description "clj-http and ring middlewares for OAuth 2.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
@@ -8,9 +5,7 @@
                  [clj-http "0.3.2"]
                  [uri "1.1.0"]
                  [commons-codec/commons-codec "1.6"]]
-  :exclusions   [org.clojure/clojure-contrib]
-  :dev-dependencies ~dev-dependencies
-  :profiles {:dev {:dependencies ~dev-dependencies}}
+  :dev-dependencies [[ring "1.4.0"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
   :aot [clj-oauth2.OAuth2Exception
         clj-oauth2.OAuth2StateMismatchException])
